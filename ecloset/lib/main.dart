c7582c2bcd2a269cd546b58691b44e165a734e3f
Smjle <denwingsa01884@gmail.com>
2023-02-17 22:03:14 +0700

    createRouteBindings

--- ecloset/lib/main.dart
+++ ecloset/lib/main.dart
@@ -28,6 +28,7 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = new MyHttpOverrides();
   await setup();
+  createRouteBindings();
   runApp(MyApp());
 }
 
