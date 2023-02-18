be141ff68d316855984ef780e17973d87dc7e13e
Smjle <denwingsa01884@gmail.com>
2023-02-18 18:49:01 +0700

    banner

--- ecloset/lib/pages/home_page.dart
+++ ecloset/lib/pages/home_page.dart
@@ -1,10 +1,18 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:ecloset/Constant/view_status.dart';
 import 'package:ecloset/Utils/routes_name.dart';
+import 'package:ecloset/ViewModel/blogs_viewModel.dart';
 import 'package:ecloset/constant/app_colors.dart';
 import 'package:ecloset/constant/app_styles.dart';
 import 'package:ecloset/Pages/closet_page.dart';
 import 'package:ecloset/Widgets/app_bar.dart';
+import 'package:ecloset/widgets/shimmer_block.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:get/get.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:shimmer/shimmer.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -21,33 +29,129 @@ class _HomePageState extends State<HomePage> {
       appBar: const MainAppBar(),
       endDrawer: const Drawer(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: ListView(children: const [
-            Padding(
-              padding: EdgeInsets.only(top: 16),
-              child: _Trending(),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: _Brand(),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: _MyCloset(),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: _Recommend(),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 24),
-              child: _Collection(),
-            ),
-          ]),
-        ),
+        child: ListView(children: const [
+          Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: banner(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: _Brand(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: _MyCloset(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: _Recommend(),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 24, left: 24, right: 24),
+            child: _Collection(),
+          ),
+        ]),
       ),
     );
+  }
+}
+
+class banner extends StatelessWidget {
+  const banner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<BlogsViewModel>(
+        model: BlogsViewModel(),
+        child: Container(
+          // color: Colors.white,
+          padding: EdgeInsets.fromLTRB(8, 0, 8, 0),
+          // padding: EdgeInsets.only(bottom: 8),
+          child: ScopedModelDescendant<BlogsViewModel>(
+            builder: (context, child, model) {
+              ViewStatus status = model.status;
+              switch (status) {
+                case ViewStatus.Loading:
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ShimmerBlock(
+                      height: (Get.width) * (747 / 1914),
+                      width: (Get.width),
+                    ),
+                  );
+                case ViewStatus.Empty:
+                case ViewStatus.Error:
+                  return SizedBox.shrink();
+                default:
+                  if (model.blogs == null || model.blogs.isEmpty) {
+                    return SizedBox.shrink();
+                  }
+                  return Container(
+                    height: (Get.width) * (747 / 1914),
+                    width: (Get.width),
+                    // margin: EdgeInsets.only(bottom: 8, top: 8),
+                    child: Swiper(
+                        onTap: (index) async {
+                          // await launch(
+                          //     "https://www.youtube.com/embed/wu32Wj_Uix4");
+                        },
+                        autoplay: model.blogs.length > 1 ? true : false,
+                        autoplayDelay: 5000,
+                        viewportFraction: 0.9,
+                        pagination: new SwiperPagination(
+                            alignment: Alignment.bottomCenter),
+                        itemCount: model.blogs.length,
+                        itemBuilder: (context, index) {
+                          if (model.blogs[index]['images'] == null ||
+                              model.blogs[index]['images'] == "")
+                            return Icon(
+                              MdiIcons.imageBroken,
+                              color: AppColors.primaryColor.withOpacity(0.5),
+                            );
+
+                          return CachedNetworkImage(
+                            imageUrl: model.blogs[index]['images'],
+                            imageBuilder: (context, imageProvider) => InkWell(
+                              onTap: () {
+                                // Get.toNamed(RouteHandler.BANNER_DETAIL,
+                                //     arguments: model.blogs[index]);
+                              },
+                              child: AspectRatio(
+                                aspectRatio: 16 / 9,
+                                child: Container(
+                                  margin: EdgeInsets.only(left: 8, right: 8),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(8),
+                                    color: Colors.blue,
+                                    image: DecorationImage(
+                                      image: imageProvider,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                    Shimmer.fromColors(
+                              baseColor: Colors.grey.shade300,
+                              highlightColor: Colors.grey.shade100,
+                              enabled: true,
+                              child: Container(
+                                color: Colors.grey,
+                              ),
+                            ),
+                            errorWidget: (context, url, error) => Icon(
+                              MdiIcons.imageBroken,
+                              color: AppColors.primaryColor.withOpacity(0.5),
+                            ),
+                          );
+                        }),
+                  );
+              }
+            },
+          ),
+        ));
   }
 }
 
@@ -326,29 +430,31 @@ class _MyCloset extends StatelessWidget {
   }
 }
 
-class _Trending extends StatelessWidget {
-  const _Trending({
-    Key? key,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        const SizedBox(
-          height: 12,
-        ),
-        AspectRatio(
-          aspectRatio: 4 / 2,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.grey,
-            ),
-          ),
-        )
-      ],
-    );
-  }
-}
+
+// class _Trending extends StatelessWidget {
+//   const _Trending({
+//     Key? key,
+//   }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       crossAxisAlignment: CrossAxisAlignment.stretch,
+//       children: [
+//         const SizedBox(
+//           height: 12,
+//         ),
+//         AspectRatio(
+//           aspectRatio: 4 / 2,
+//           child: Container(
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(10),
+//               color: Colors.grey,
+//             ),
+//           ),
+//         )
+//       ],
+//     );
+//   }
+// }
