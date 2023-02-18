2478a390cc12c73d7a3c322efebc63f7fa913c0c
LBCrayon <baophamtranle@gmail.com>
2023-02-18 12:00:17 +0700

    setting page

--- ecloset/lib/setup.dart
+++ ecloset/lib/setup.dart
@@ -1,6 +1,8 @@
 import 'package:ecloset/ViewModel/closet_viewModel.dart';
 import 'package:ecloset/ViewModel/root_viewModel.dart';
 import 'package:ecloset/firebase_options.dart';
+import 'package:ecloset/pages/settings/setting_page.dart';
+import 'package:ecloset/pages/user_profile_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 
@@ -13,4 +15,6 @@ Future setup() async {
 void createRouteBindings() async {
   Get.put(RootViewModel());
   Get.put(ClosetViewModel());
+  Get.put(const SettingPage());
+  Get.put(const UserProfilePage());
 }
