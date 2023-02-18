30f948b74a54ac4fcba45f8630da42afbb1d889d
Le Thanh Phuoc <thanhphuoc.se@gmail.com>
2023-02-18 17:36:03 +0700

    change bottom bar

--- ecloset/lib/pages/app.dart
+++ ecloset/lib/pages/app.dart
@@ -28,37 +28,94 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _screens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            tooltip: "",
-            icon: Icon(Icons.home),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            tooltip: "",
-            icon: Icon(Icons.abc),
-            label: "Abc",
-          ),
-          BottomNavigationBarItem(
-            tooltip: "",
-            icon: Icon(Icons.abc),
-            label: "Abc",
-          ),
-          BottomNavigationBarItem(
-            tooltip: "",
-            icon: Icon(Icons.people),
-            label: "Profile",
-          ),
-        ],
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: AppColors.primaryColor,
-        selectedItemColor: AppColors.secondaryColor,
-        unselectedItemColor: AppColors.textWhite,
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+        elevation: 2.0,
       ),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(), //shape of notch
+        notchMargin: 5,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.home,
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedIndex = 0;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.abc,
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedIndex = 1;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.abc,
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedIndex = 2;
+                });
+              },
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.people,
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedIndex = 3;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       tooltip: "",
+      //       icon: Icon(Icons.home),
+      //       label: "Home",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       tooltip: "",
+      //       icon: Icon(Icons.abc),
+      //       label: "Abc",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       tooltip: "",
+      //       icon: Icon(Icons.abc),
+      //       label: "Abc",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       tooltip: "",
+      //       icon: Icon(Icons.people),
+      //       label: "Profile",
+      //     ),
+      //   ],
+      //   type: BottomNavigationBarType.fixed,
+      //   backgroundColor: AppColors.primaryColor,
+      //   selectedItemColor: AppColors.secondaryColor,
+      //   unselectedItemColor: AppColors.textWhite,
+      //   currentIndex: _selectedIndex,
+      //   onTap: _onItemTapped,
+      // ),
     );
   }
 }
